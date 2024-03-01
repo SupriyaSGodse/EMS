bd8747e71fa56856a8f71de8059de5285b8a5c71
Supriya <supriyasg2911@gmail.com>
2024-03-02 01:42:57 +0530

    Added Components for Employee and Department Management

--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { listEmployees } from '../services/EmployeeService';
+import { deleteEmployee, listEmployees } from '../services/EmployeeService';
 import { useNavigate } from 'react-router-dom';
 
 const ListEmployeeComponent = () => {
@@ -8,15 +8,32 @@ const ListEmployeeComponent = () => {
   const navigator = useNavigate();
 
   useEffect(() => {
+    getAllEmployees();
+  }, []);
+
+  function getAllEmployees() {
     listEmployees()
       .then((response) => {
         setEmployees(response.data);
       })
       .catch((error) => console.error(error));
-  }, []);
+  }
 
   function addNewEmployee() {
     navigator('/add-employee');
+  }
+
+  function updateEmployee(id) {
+    navigator(`/edit-employee/${id}`);
+  }
+  function removeEmployee(id) {
+    deleteEmployee(id)
+      .then((response) => {
+        getAllEmployees();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   return (
@@ -32,6 +49,7 @@ const ListEmployeeComponent = () => {
             <th> Employee First Name</th>
             <th> Employee Last Name</th>
             <th> Employee Email Id</th>
+            <th> Actions</th>
           </tr>
         </thead>
         <tbody>
@@ -41,6 +59,21 @@ const ListEmployeeComponent = () => {
               <td>{employee.firstName}</td>
               <td>{employee.lastName}</td>
               <td>{employee.email}</td>
+              <td>
+                <button
+                  className="btn btn-info"
+                  onClick={() => updateEmployee(employee.id)}
+                >
+                  Update
+                </button>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => removeEmployee(employee.id)}
+                  style={{ marginLeft: '10px' }}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
           <tr></tr>
